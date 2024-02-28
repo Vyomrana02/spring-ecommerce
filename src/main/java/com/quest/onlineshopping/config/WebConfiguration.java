455c835dc493f3020e568ae46fe776c9f51f9256
Vyom Rana <vyom.rana02@gmail.com>
2024-02-28 16:51:04 +0530

    Update WebConfiguration.java

--- src/main/java/com/quest/onlineshopping/config/WebConfiguration.java
+++ src/main/java/com/quest/onlineshopping/config/WebConfiguration.java
@@ -8,7 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
- 
+
+    // @Bean is used to get object of 3rd party , so ReloadableResourceBundleMessageSource is 3rd party to have validation.
     @Bean
     public MessageSource messageSource() {
         ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
